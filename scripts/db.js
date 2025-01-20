4ed85623e953bafef77fb95da4a07e49cd40ee1b
Stephanie Englberger <stephaniekoebich@aol.com>
2025-01-20 21:15:28 +0100

    add JSON array myDishes

---
+++ scripts/db.js
@@ -0,0 +1,90 @@
+let myDishes = [
+  {
+    category: "Hauptgerichte",
+    items: [
+      {
+        name: "Exotische Gemüsepfanne",
+        price: 14.9,
+        description: "mit Tofu, getrockneten Aprikosen, Linsen und Paprika",
+      },
+      {
+        name: "Linguine mit asiatischer Kürbissauce",
+        price: 13.5,
+        description: "und marinierten Shiitake-Pilzen",
+      },
+      {
+        name: "Di San Xian",
+        price: 15.8,
+        description:
+          "Chinesisches Gericht mit frittierten Kartoffel- und Auberginenwürfeln",
+      },
+      {
+        name: "Reisgericht mit Cashewkernen",
+        price: 14.8,
+        description: "und Aprikosen in Currysauce",
+      },
+    ],
+  },
+  {
+    category: "Beilagen",
+    items: [
+      {
+        name: "Geröstete Cashewkerne",
+        price: 3.5,
+      },
+      {
+        name: "Minz-Joghurtsoße",
+        price: 2.8,
+      },
+      {
+        name: "Knusprige Salbeiblätter",
+        price: 3.2,
+      },
+    ],
+  },
+  {
+    category: "Nachspeisen",
+    items: [
+      {
+        id: 101,
+        name: "Kokos Panna cotta",
+        price: 6.9,
+        description: "mit marinierten Mangos",
+      },
+      {
+        name: "Gebackene Ananasringe",
+        price: 5.8,
+      },
+      {
+        name: "Exotischer Wintersalat",
+        price: 7.5,
+        description: "mit Müsli-Crunch",
+      },
+    ],
+  },
+  {
+    category: "Getränke",
+    items: [
+      {
+        name: "Ingwer-Kokosmilch-Drink",
+        price: 4.5,
+      },
+      {
+        name: "Frische Zitronen-Minze-Limonade",
+        price: 3.8,
+      },
+      {
+        name: "Asiatischer Kräutertee",
+        price: 3.2,
+      },
+      {
+        name: "Tropischer Aprikosen-Smoothie",
+        price: 4.9,
+      },
+      {
+        name: "Kokos-Ingwer-Wasser",
+        price: 3.5,
+      },
+    ],
+  }
+];
